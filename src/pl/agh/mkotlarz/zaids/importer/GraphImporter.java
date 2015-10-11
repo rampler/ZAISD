82c262e867c9303a1bb2ed148fe4d4d9a18d54dc
rampler <rampler@gmail.com>
2015-10-11 20:55:24 +0200

    Importer from console, string and file done

---
+++ src/pl/agh/mkotlarz/zaids/importer/GraphImporter.java
@@ -0,0 +1,58 @@
+package pl.agh.mkotlarz.zaids.importer;
+
+import pl.agh.mkotlarz.zaids.graph.Graph;
+import pl.agh.mkotlarz.zaids.graph.GraphEdge;
+import pl.agh.mkotlarz.zaids.graph.GraphNode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by Mateusz on 11.10.2015.
+ */
+public class GraphImporter {
+
+    /* Private methods */
+
+    private void proceedNextLine(Graph graph, String line) {
+        line = line.replaceAll("\\s+","");
+        String lines[] = line.split(";");
+        GraphNode firstNode = new GraphNode(Integer.parseInt(lines[0]));
+        GraphNode secondNode = new GraphNode(Integer.parseInt(lines[1]));
+        graph.addNode(firstNode);
+        graph.addNode(secondNode);
+        graph.addEdge(new GraphEdge(firstNode, secondNode, Integer.parseInt(lines[2])));
+    }
+
+
+    /* Public methods */
+
+    public Graph importGraphFromConsole(Graph graph) {
+        System.out.println("Write new edges in lines in format:    firstNodeId; secondNodeId; edgeWeight");
+        System.out.println("To end creating graph put empty line.");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while(line != null && !line.replaceAll("\\s+","").equals("")) {
+            proceedNextLine(graph, line);
+            line = scanner.nextLine();
+        }
+        scanner.close();
+        return graph;
+    }
+
+    public Graph importGraphFromString(Graph graph, String graphString) {
+        String lines[] = graphString.split("\\r?\\n");
+        for(String line : lines)
+            proceedNextLine(graph, line);
+        return graph;
+    }
+
+    public Graph importGraphFromFile(Graph graph, String path) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(path));
+        while(scanner.hasNextLine())
+            proceedNextLine(graph, scanner.nextLine());
+        scanner.close();
+        return graph;
+    }
+}
